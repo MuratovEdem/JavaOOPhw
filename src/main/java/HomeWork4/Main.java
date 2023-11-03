9b606104dee0ff320ea49d742df8d4c9c272eecb
Edem <edem.muratov01@mail.ru>
2023-11-03 14:48:22 +0300

    HomeWork4

--- src/main/java/HomeWork4/Main.java
+++ src/main/java/HomeWork4/Main.java
@@ -8,23 +8,23 @@ public class Main {
         appleBox.add(new Apple(2)); // Должно компилироваться
         appleBox.add(new Apple(4)); // Должно компилироваться
         System.out.println(appleBox.getWeight()); // 6
-//        appleBox.add(new Orange(4)); // Не должно компилироваться!!!
+        appleBox.add(new Orange(4)); // Не должно компилироваться!!!
         appleBox.add(new GoldenApple(4)); // Должно компилироваться
         System.out.println(appleBox.getWeight()); // 10
 
         Box<Orange> orangeBox = new Box<>();
-//        orangeBox.add(new Apple(2)); // Не должно компилироваться!!!
+        orangeBox.add(new Apple(2)); // Не должно компилироваться!!!
         orangeBox.add(new Orange(8)); // Должно компилироваться
         System.out.println(orangeBox.getWeight()); // 8
 
-//        orangeBox.move(appleBox); // Не должно компилироваться!!!
-//        appleBox.move(orangeBox); // Не должно компилироваться!!!
+        orangeBox.move(appleBox); // Не должно компилироваться!!!
+        appleBox.move(orangeBox); // Не должно компилироваться!!!
 
         Box<GoldenApple> goldenAppleBox = new Box<>();
         goldenAppleBox.add(new GoldenApple(20)); // Должно компилироваться
-//        goldenAppleBox.add(new Apple(20)); // Не должно компилироваться!!!
+        goldenAppleBox.add(new Apple(20)); // Не должно компилироваться!!!
 
-//        appleBox.move(goldenAppleBox); // Не должно компилироваться!!!
+        appleBox.move(goldenAppleBox); // Не должно компилироваться!!!
         goldenAppleBox.move(appleBox); // Должно компилироваться
 
         System.out.println(goldenAppleBox.getWeight()); // 20
